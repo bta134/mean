876505ebc23cadfd0b0963a0165d73ba061e63fc
bta134@gmail.com <bta134@gmail.com>
2019-01-23 11:50:01 +0700

    uilab

---
+++ uilab/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DraganddropComponent } from './draganddrop/draganddrop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DraganddropComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
